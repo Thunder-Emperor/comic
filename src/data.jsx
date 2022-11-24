3dafe2a14d88aa8f3fe4b1259f80a60f26448f7f
Thunder-Emperor <bellerophonpegasus29@gmail.com>
2022-11-24 14:42:10 +0530

    add description in data

--- src/data.jsx
+++ src/data.jsx
@@ -2,9 +2,9 @@ import btd from "../src/images/before-the-dawn.jpg"
 import nwb from '../src/images/nowhere-boys.png'
 import skyward from '../src/images/skyward.jpg'
 import laketown from '../src/images/lake-town.jpg'
-import riseofthesun from './images/rise-of-the-red-sum.jpg'
+import drstone from './images/dr_stone.jpg'
 import granmuerto from './images/loisel.jpg'
-import timetopretend from './images/time-to-pretend.jpg'
+import wolf from './images/wolf.png'
 import across from './images/across-a-field-of-starlight.jpg'
 
 const data = [
@@ -19,43 +19,50 @@ const data = [
         id:2,
         name:'NOWHERE BOYS',
         image:nwb,
-        price:3
+        price:3,
+        description:' about a teenage Syrian refugee whose father dies on the journey to Europe—in part to help her kids, complex topics like immigrant integration, terrorism and the refugee crisis.'
     },
     {
         id:3,
         name:'SKYWARD',
         image:skyward,
-        price:3
+        price:3,
+        description: "One day, gravity on earth suddenly became a fraction of what it is now. later, humanity has adapted to its,it's…well, it's pretty awesome, actually"
     },
     {
         id:4,
         name:'LAKE TOWN',
         image:laketown,
-        price:3
+        price:3,
+        description:"Lake-town, or Esgaroth (known fully as 'Esgaroth upon the Long Lake'), was a small settlement of Men in the northwestern part of the Long Lake in Rhovanion. The town was"
     },
     {
         id:5,
-        name:'RISE OF THE RED SUN',
-        image:riseofthesun,
-        price:3
+        name:'DR. STONE',
+        image:drstone,
+        price:3,
+        description:' teenage scientific genius Senku Ishigami, who plans to rebuild civilization after humanity was mysteriously petrified for 3,700 years.'
     },
     {
         id:6,
         name:'EL GRAN MUERTO',
         image:granmuerto,
-        price:3
+        price:3,
+        description:'Hilando Libros es una Librería online situada en Madrid (España) creada en el año 2013. A través de ella vendemos libros nuevos y usados. Queremos atenderle lo mejor posible... '
     },
     {
         id:7,
-        name:'TIME TO PRETEND',
-        image:timetopretend,
-        price:3
+        name:'WOLF',
+        image:wolf,
+        price:3,
+        description:'Five years later: Antoine Wolfe is in prison, Anita Christ is nearly eighteen years old, and the apocalypse is coming. As their paths ...'
     },
     {
         id:8,
         name:'ACROSS A FIELD OF STARLIGHT',
         image:across,
-        price:3
+        price:3,
+        description:'An epic sci-fi graphic novel romance between two non-binary characters as they find one another through time, distance, and war. ...'
     },
 
 ]
